f7fb85f3617185e94f1dc51310c4345a2180f376
Igor Saric <igor.saric@outlook.com>
2024-04-29 01:41:34 +0200

    a

--- scripts/camera.sh
+++ scripts/camera.sh
@@ -4,5 +4,6 @@ _ip="192.168.0.30"
 
 ssh "${_ip}" '/home/igor/hid/scripts/server.sh'
 printf 'Waiting for libcamera-vid to settle...\n'
-sleep 5
-ffplay "tcp://${_ip}:8494" -fflags nobuffer -flags low_delay -framedrop
+sleep 4
+printf 'Starting ffplay...\n'
+ffplay -loglevel error -fflags nobuffer -flags low_delay -framedrop -i "tcp://${_ip}:8494"
