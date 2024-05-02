cc8c73f856d61162a719aff638fff46d41488b75
Igor Saric <igor.saric@outlook.com>
2024-05-03 00:42:03 +0200

    a

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 
-( libcamera-vid -t 0 --width 1280 --height 720 --framerate 30 --listen -o tcp://0.0.0.0:8494 & ) > /dev/null 2>&1
+( libcamera-vid -t 0 --awb indoor --width 1280 --height 720 --framerate 30 --listen -o tcp://0.0.0.0:8494 & ) > /dev/null 2>&1
