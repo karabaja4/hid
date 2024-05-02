692248c687ec3f0a6843230f4130e060372ba8fb
Igor Saric <igor.saric@outlook.com>
2024-05-03 01:41:38 +0200

    a

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 set -eu
 
+#!/bin/sh
+set -eu
+
+if pgrep -x 'libcamera-vid' > /dev/null
+then
+    printf 'libcamera-vid is already running.\n'
+    exit 1
+fi
+
 ( libcamera-vid -t 0 --awbgains 1,1 --width 1280 --height 720 --framerate 30 --listen -o tcp://0.0.0.0:8494 & ) > /dev/null 2>&1
