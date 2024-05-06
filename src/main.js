5f8af3ba3cc66182fdd405fae1d07ffcf56722c8
Igor Saric <igor.saric@outlook.com>
2024-05-07 00:28:10 +0200

    a

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ const init = async () => {
       throw new Error('Release sequence not found.');
     }
   } catch (err) {
-    log.info(`Error writing to ${hidPath} (${err?.code}), reconnecting...`);
+    log.info(`Error writing to ${hidPath} (${err?.code} - ${err?.message}), reconnecting...`);
     const scriptPath = path.join(__dirname, '../config/hid.sh');
     const output = await exec(`doas /bin/sh ${scriptPath}`);
     log.bash(output?.stdout?.trim());
