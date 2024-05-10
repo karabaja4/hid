78bce1ebb223dc54642c92efc4198bc7bc365cf8
Igor Saric <karabaja4@gmail.com>
2024-05-11 00:48:34 +0200

    a

--- scripts/client.sh
+++ scripts/client.sh
@@ -2,8 +2,10 @@
 set -eu
 
 _ip="192.168.0.30"
+_port="28801"
+_user="igor"
 
-ssh "${_ip}" '/home/igor/hid/scripts/server.sh'
+ssh -l "${_user}" -p "${_port}" "${_ip}" '/home/igor/hid/scripts/server.sh'
 
 printf 'Waiting for rpicam-vid to settle...\n'
 sleep 3
@@ -12,4 +14,4 @@ printf 'Starting ffplay...\n'
 ( ffplay -loglevel fatal -strict experimental -vf setpts=0 -fflags nobuffer -flags low_delay -framedrop -i "tcp://${_ip}:8494" & ) > /dev/null 2>&1
 
 printf 'Starting keyboard...\n'
-ssh "${_ip}" -t 'node /home/igor/hid/src/main.js'
+ssh -l "${_user}" -p "${_port}" "${_ip}" -t 'node /home/igor/hid/src/main.js'
