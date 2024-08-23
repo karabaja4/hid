a009c5d3de374b6e9799622cb510e740af1e6cf9
Igor Saric <karabaja4@gmail.com>
2024-08-24 00:19:04 +0200

    a

--- scripts/client.sh
+++ scripts/client.sh
@@ -2,25 +2,26 @@
 set -eu
 _fn="$(basename "${0}")"
 
+_user="igor"
 _ip="localhost"
 _port="52222"
-_user="igor"
+_info="${_user}@${_ip}:${_port}"
 _server="tcp://localhost:58494"
 
 _log() {
     printf '[\033[36m%s\033[0m] %s\n' "${_fn}" "${1}"
 }
 
-_log 'Starting server...'
+_log "Starting rpicam-vid server on ${_info}"
 ssh -l "${_user}" -p "${_port}" "${_ip}" '/home/igor/hid/scripts/server.sh'
 
-_log 'Waiting for rpicam-vid to settle...'
+_log "Waiting for rpicam-vid to settle on ${_info}"
 sleep 3
 
-_log "Connecting ffplay to ${_server}..."
+_log "Connecting ffplay to ${_server}"
 ( ffplay -loglevel fatal -strict experimental -vf setpts=0 -fflags nobuffer -flags low_delay -framedrop -i "${_server}" & ) > /dev/null 2>&1
 
-_log 'Starting keyboard...'
+_log "Starting keyboard emulator on ${_info}"
 ssh -l "${_user}" -p "${_port}" "${_ip}" -t 'node /home/igor/hid/src/main.js'
 
 _log 'Exited.'
