d8bb2afa1144ecfd1416e67043acf1ae3462cd8e
Igor Saric <karabaja4@gmail.com>
2025-04-08 22:26:19 +0200

    a

--- scripts/server.sh
+++ scripts/server.sh
@@ -32,5 +32,6 @@ _log "Waiting for ${_name} to settle"
 sleep 5
 
 _log "Focusing camera"
+v4l2-ctl --device "${_camera}" --set-ctrl=auto_exposure=0
 v4l2-ctl --device "${_camera}" --set-ctrl=focus_automatic_continuous=0
 v4l2-ctl --device "${_camera}" --set-ctrl=focus_absolute=90
